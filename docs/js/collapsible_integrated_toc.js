3321b95a551f92d987b87e88a35b5f4a9c92ef4c
Aiden G <emailaiden@gmail.com>
2024-11-19 21:53:06 +0200

    test js

--- docs/js/collapsible_integrated_toc.js
+++ docs/js/collapsible_integrated_toc.js
@@ -2,5 +2,21 @@ $(document).ready(function() {
 
     console.log('worked');
 
+    const toggleSpan = "<span class='md-nav__icon md-icon'></span>";
+
+    function recursiveParentMod(i, item) {
+        if ($(item).children('nav').length) {
+            $(item).addClass(".md-nav__item--nested");
+            $(item).children('nav').children('ul').children('li').each(recursiveParentMod);
+            $(item).children("label").append($(toggleSpan));
+        }
+    }
+
+    $(".md-nav--primary > ul > .md-nav__item--nested").each(function(i, p) {
+        $(p).children('label').append($(toggleSpan));
+        $(p).children('nav').children('ul').children('li').each(function(j, c) {
+            recursiveParentMod(j, c);
+        })
+    });
 
 });
