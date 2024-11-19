51b44838964be8e4e6ce18f20ebe84fa538136db
Aiden G <emailaiden@gmail.com>
2024-11-19 21:58:41 +0200

    test

--- docs/js/collapsible_integrated_toc.js
+++ docs/js/collapsible_integrated_toc.js
@@ -8,12 +8,12 @@ $(document).ready(function() {
         if ($(item).children('nav').length) {
             $(item).addClass(".md-nav__item--nested");
             $(item).children('nav').children('ul').children('li').each(recursiveParentMod);
-            $(item).children("label").append($(toggleSpan));
+            $(item).children("a").append($(toggleSpan));
         }
     }
 
     $(".md-nav--primary > ul > .md-nav__item--nested").each(function(i, p) {
-        $(p).children('label').append($(toggleSpan));
+        // $(p).children('label').append($(toggleSpan));
         $(p).children('nav').children('ul').children('li').each(function(j, c) {
             recursiveParentMod(j, c);
         })
