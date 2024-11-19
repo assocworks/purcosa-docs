8f50eaa1d87e1095f20458f6cc2640389b36bf74
Aiden G <emailaiden@gmail.com>
2024-11-19 22:14:18 +0200

    Update collapsible_integrated_toc.js

--- docs/js/collapsible_integrated_toc.js
+++ docs/js/collapsible_integrated_toc.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
     console.log('worked');
 
     const toggleSpan = "<span class='md-nav__icon md-icon'></span>";
+    const toggleBox = "<input class='md-nav__toggle md-toggle' type='checkbox'>";
 
     function recursiveParentMod(i, item) {
         if ($(item).children('nav').length) {
-            $(item).addClass(".md-nav__item--nested");
+            $(item).addClass("md-nav__item--nested");
             $(item).children('nav').children('ul').children('li').each(recursiveParentMod);
             $(item).children("a").append($(toggleSpan));
+            $(item).append($(toggleBox));
         }
     }
 
